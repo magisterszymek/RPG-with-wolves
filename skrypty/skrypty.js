a913a2a971a9e1f96f02e49170437d98cc76862c
magisterszymek <35369564+magisterszymek@users.noreply.github.com>
2020-02-24 19:36:31 +0100

    początkowy projek wyglądu

---
+++ skrypty/skrypty.js
@@ -0,0 +1,13 @@
+var testt=0;// nie bij za ten kod, to tak tylko na pocz¹tek XD
+function test(){
+	if(testt==0){
+	document.getElementById("jeden").style.zIndex="-1";
+	document.getElementById("dwa").style.zIndex="0";
+	testt=1;
+	}	
+	else{
+	document.getElementById("jeden").style.zIndex="0";
+	document.getElementById("dwa").style.zIndex="-1";
+	testt=0
+	}
+}
