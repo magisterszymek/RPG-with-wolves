71e6b7edfac451d904965f079c9cdb7eba719501
magisterszymek <35369564+magisterszymek@users.noreply.github.com>
2020-10-11 12:47:17 +0200

    Zmiana zakładki "walka" na "przygody"

--- skrypty/skrypty.js
+++ skrypty/skrypty.js
@@ -63,34 +63,35 @@
 		
 // ---------- Koniec zmiennych ----------
 
-function zakladka(NrZakladki){
+function zakladka(NrZakladki) {
+	var zakladki = ['zakladkaPrzygody', 'zakladkaEkwipunek', 'zakladkaMenu', 'zakladkaUstawienia'];
 	switch(NrZakladki){
 		case 0:{
-		document.getElementById("zakladkaWalka").style.zIndex="0";
-		document.getElementById("zakladkaEkwipunek").style.zIndex="-1";
-		document.getElementById("zakladkaMenu").style.zIndex="-1";
-		document.getElementById("zakladkaUstawienia").style.zIndex="-1";
+		document.getElementById(zakladki[0]).style.zIndex="0";
+		document.getElementById(zakladki[1]).style.zIndex="-1";
+		document.getElementById(zakladki[2]).style.zIndex="-1";
+		document.getElementById(zakladki[3]).style.zIndex="-1";
 		break;
 		}
 		case 1:{
-		document.getElementById("zakladkaWalka").style.zIndex="-1";
-		document.getElementById("zakladkaEkwipunek").style.zIndex="0";
-		document.getElementById("zakladkaMenu").style.zIndex="-1";
-		document.getElementById("zakladkaUstawienia").style.zIndex="-1";
+		document.getElementById(zakladki[0]).style.zIndex="-1";
+		document.getElementById(zakladki[1]).style.zIndex="0";
+		document.getElementById(zakladki[2]).style.zIndex="-1";
+		document.getElementById(zakladki[3]).style.zIndex="-1";
 		break;
 		}
 		case 2:{
-		document.getElementById("zakladkaWalka").style.zIndex="-1";
-		document.getElementById("zakladkaEkwipunek").style.zIndex="-1";
-		document.getElementById("zakladkaMenu").style.zIndex="0";
-		document.getElementById("zakladkaUstawienia").style.zIndex="-1";
+		document.getElementById(zakladki[0]).style.zIndex="-1";
+		document.getElementById(zakladki[1]).style.zIndex="-1";
+		document.getElementById(zakladki[2]).style.zIndex="0";
+		document.getElementById(zakladki[3]).style.zIndex="-1";
 		break;
 		}
 		case 3:{
-		document.getElementById("zakladkaWalka").style.zIndex="-1";
-		document.getElementById("zakladkaEkwipunek").style.zIndex="-1";
-		document.getElementById("zakladkaMenu").style.zIndex="-1";
-		document.getElementById("zakladkaUstawienia").style.zIndex="0";
+		document.getElementById(zakladki[0]).style.zIndex="-1";
+		document.getElementById(zakladki[1]).style.zIndex="-1";
+		document.getElementById(zakladki[2]).style.zIndex="-1";
+		document.getElementById(zakladki[3]).style.zIndex="0";
 		break;
 		}
 	}
