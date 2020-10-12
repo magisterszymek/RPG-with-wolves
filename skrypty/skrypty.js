02f7c955aba8e968dd59f4de4dfb63f08c63fa7d
Maciej Świątek <42539603+Maciej-Swiatek@users.noreply.github.com>
2020-10-12 15:26:07 +0200

    [EKWIPUNEK] Poprawa ekwipunku

--- skrypty/skrypty.js
+++ skrypty/skrypty.js
@@ -421,17 +421,15 @@ wybierzCios("zwykly");
 // ----- Funkcje ekwipunku -----
 
 function allowDrop(ev) {
-	ev.preventDefault();
+  ev.preventDefault();
 }
 
 function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
-	bufor = ev.target.id;
-	buforDrugi = ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
+function drop(ev, el) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  el.appendChild(document.getElementById(data));
 }
