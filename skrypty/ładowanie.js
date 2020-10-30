9525efc0573bef7bf4818f021e31921a604a9527
Maciej Świątek <42539603+Maciej-Swiatek@users.noreply.github.com>
2020-10-30 23:43:54 +0100

    Ekran podróży

--- skrypty/ładowanie.js
+++ skrypty/ładowanie.js
@@ -1,14 +1,31 @@
 //Ładowanie grafik
+
+//Ekran ładowania
 div = document.createElement("DIV");
 image = document.getElementById("ladowanieZdjecie");
-div.style.backgroundImage = "url('Obrazy/Ładowanie.png')";
+div.style.backgroundImage = "url('Obrazy/Ładowanie/Ładowanie.png')";
 div.style.backgroundPosition = "center";
 div.style.backgroundRepeat = "no-repeat";
 div.style.backgroundSize = "cover";
-div.style.zIndex = "10";
+div.style.zIndex = "2";
 div.className = "wyborNicku";
 div.id = "ladowanie";
 document.body.appendChild(div);
+
+//Ekran podróży
+div = document.createElement("DIV");
+image = document.getElementById("ladowanieZdjecie");
+div.style.backgroundImage = "url('Obrazy/Ładowanie/Podróż.png')";
+div.style.backgroundPosition = "center";
+div.style.backgroundRepeat = "no-repeat";
+div.style.backgroundSize = "100%";
+div.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+div.style.zIndex = "1";
+div.className = "wyborNicku";
+div.id = "podroz";
+document.body.appendChild(div);
+setTimeout(function(){ document.getElementById("podroz").setAttribute("hidden", ""); }, 1);
+
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Mapa/Mapa.png')"; }, 1010);
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Mapa/PradawnyLas.png')"; }, 2020);
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Mapa/ZrujnowanyObóz.png')"; }, 2030);
@@ -17,15 +34,5 @@ setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Tła/GrzyboweP
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Tła/PradawnyLas.png')"; }, 2060);
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Tła/WieżaMaga.png')"; }, 2070);
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Tła/ZrujnowanyObóz.png')"; }, 2080);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Drwal.png')"; }, 2090);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Kowal.png')"; }, 2100);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Leżące_drzewo.png')"; }, 2110);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Magiczne_pole.png')"; }, 2120);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Grzybiarz.png')"; }, 2130);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Pułapka.png')"; }, 2140);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Myśliwy.png')"; }, 2150);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Młody_myśliwy.png')"; }, 2160);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Szczurzy_strażnik.png')"; }, 2170);
-setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Przeciwnicy/Wieśniak.png')"; }, 2180);
 setTimeout(function(){ image.style.backgroundImage = "url('Obrazy/Postacie/Wilk.png')"; }, 2190);
 setTimeout(function(){ document.getElementById("ladowanie").setAttribute("hidden", ""); }, 3000);
